e8c2deae0825efe0d1e49fc32764edfb2aaf5af0
Anders Clausen <anders.clausen1@maersk.com>
2021-02-15 14:52:13 +0000

    Initial commit

---
+++ src/test/java/com/example/contenttypeerror/ContentTypeErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contenttypeerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentTypeErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
